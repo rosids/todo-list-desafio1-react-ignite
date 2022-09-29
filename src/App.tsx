cc7f7319aacd4fc87bc9a25673684c1a98635965
Rosiele David <rosieleserpa@hotmail.com>
2022-09-29 20:57:58 -0300

    add responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Header } from './components/Header';
 import { NewTask } from './components/NewTask';
 
+import styles from './App.module.css';
+
 import './global.css';
 
 function App() {
   return (
     <div>
       <Header />
-
-      <NewTask />
+      <div className={styles.wrapper}>
+        <NewTask />
+      </div>
     </div>
   )
 }
