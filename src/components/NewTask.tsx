4e18bc30f76f989f627e487b5b46b8983f4d41b2
Rosiele David <rosieleserpa@hotmail.com>
2022-10-04 15:27:18 -0300

    add validação, criação e remoção de tarefa

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,16 +1,60 @@
 import { PlusCircle } from 'phosphor-react';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 
 import { TaskEmpty } from './TaskEmpty';
 import { TaskHeader } from './TaskHeader';
+import { Task } from './Task';
 
 import styles from './NewTask.module.css';
 
+export interface Task {
+  id: number
+  content: string;
+}
+
 export function NewTask() {
+  const [newTaskText, setNewTaskText] = useState('');
+  const [tasks, setTasks] = useState(Array<Task>);
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    const id = (tasks.at(-1)?.id || tasks.length ) + 1;
+    setTasks((state) => [...state, { id, content: newTaskText } ]);
+
+    setNewTaskText('');
+  }
+
+  function handleNewTaskTextChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('');
+    setNewTaskText(event.target.value);
+  }
+
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('Digite uma tarefa!');
+  }
+
+  function deleteTask(taskToDelete: number) {
+    const tasksWithoutDeleteOne = tasks.filter(task => task.id !== taskToDelete);
+
+    setTasks(tasksWithoutDeleteOne);
+  }
+
+  const isTaskListEmpty = tasks.length === 0;
+
+  const isNewTaskListEmpty = newTaskText.length === 0;
+
   return (
     <>
-      <form className={styles.inputContainer}>
-        <input placeholder="Adicione uma nova tarefa" />
-        <button type="submit">
+      <form onSubmit={handleCreateNewTask} className={styles.inputContainer}>
+        <input
+          placeholder="Adicione uma nova tarefa"
+          value={newTaskText}
+          onChange={handleNewTaskTextChange}
+          onInvalid={handleNewTaskInvalid}
+          required
+        />
+        <button type="submit" disabled={isNewTaskListEmpty}>
           Criar
           <PlusCircle size={16} />
         </button>
@@ -19,7 +63,18 @@ export function NewTask() {
       <div className={styles.taskList}>
         <div className={styles.task}>
           <TaskHeader />
-          <TaskEmpty />
+          {isTaskListEmpty ? (
+            <TaskEmpty />
+          ) : (
+            tasks.map(task => (
+              <Task
+                key={task.id}
+                id={task.id}
+                content={task.content}
+                onDeleteTask={deleteTask}
+              />
+            ))
+          )}
         </div>
       </div>
     </>
