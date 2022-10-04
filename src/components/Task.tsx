08331d73f24d81d3e0d1e8c280b3ef1cee5c3e17
Rosiele David <rosieleserpa@hotmail.com>
2022-10-04 15:22:02 -0300

    corrige componente Task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,22 +1,25 @@
-import { ClipboardText } from 'phosphor-react';
+import { Trash } from 'phosphor-react';
 
 import styles from './Task.module.css';
 
-export function Task() {
-  return (
-    <div className={styles.task}>
-      <div className={styles.info}>
-        <p className={styles.created}>Tarefas criadas <span>0</span></p>
-        <p className={styles.done}>Concluídas <span>0</span></p>
-      </div>
+import { Task as TaskP } from './NewTask';
 
-      <div className={styles.todoEmpty}>
-        <ClipboardText size={56} weight="bold" />
-        <p>
-          Você ainda não tem tarefas cadastradas
-          <span>Crie tarefas e organize seus itens a fazer</span>
-        </p>
-      </div>
+interface TaskProps extends TaskP {
+  onDeleteTask: (task: number) => void;
+}
+
+export function Task({ id, content, onDeleteTask }: TaskProps) {
+  function handleDeleteTask () {
+    onDeleteTask(id);
+  }
+
+  return (
+    <div className={styles.taskList}>
+      <input type="checkbox" />
+      <p>{content}</p>
+      <button onClick={handleDeleteTask}>
+        <Trash size={24} />
+      </button>
     </div>
-  )
+  );
 }
